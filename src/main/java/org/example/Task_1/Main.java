82ae6b193369a32044182cb58737f0a770532d58
Rohan Chourasiya <rohan.chourasiya@webkorps.com>
2025-10-15 16:41:44 +0530

    First Code MultiThreading

---
+++ src/main/java/org/example/Task_1/Main.java
@@ -0,0 +1,47 @@
+package org.example.Task_1;
+
+class Main {
+   public static void main(String[] args) {
+
+       Thread number =  new Thread(() ->{
+           for(int i=1;i<=5;i++){
+               System.out.println(Thread.currentThread().getName() + " -> "+i);
+           }
+           try {
+               Thread.sleep(500);
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           }
+       });
+
+       Thread alphabet =  new Thread(() ->{
+           for(char ch='A';ch<='E';ch++){
+               System.out.println(Thread.currentThread().getName() + " -> "+ch);
+           }
+           try {
+               Thread.sleep(700);
+           } catch (InterruptedException e) {
+               throw new RuntimeException(e);
+           }
+       });
+
+       number.setName("NumberThread");
+       alphabet.setName("AlphabetThread");
+
+       number.setPriority(Thread.MAX_PRIORITY);
+       alphabet.setPriority(Thread.MIN_PRIORITY);
+
+       number.start();
+       alphabet.start();
+
+       try {
+           number.join();
+           alphabet.join();
+       } catch (InterruptedException e) {
+           throw new RuntimeException(e);
+       }
+
+       System.out.println("All Thread are Finished the Execution.");
+
+    }
+}
