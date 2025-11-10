54a1ca5f4bb44bb324a182610730d8419697b14c
Rohan Chourasiya <rohan.chourasiya@webkorps.com>
2025-11-10 10:43:15 +0530

    task

--- src/main/java/Sync_Async_Example/Async/CompletableFuture/RunAsyncExample.java
+++ src/main/java/Sync_Async_Example/Async/CompletableFuture/RunAsyncExample.java
@@ -3,10 +3,11 @@ package Sync_Async_Example.Async.CompletableFuture;
 import java.util.concurrent.CompletableFuture;
 
 public class RunAsyncExample {
-    static void main(String[] args) {
-        CompletableFuture.runAsync(()->{
+    public static void main(String[] args) throws InterruptedException {
+        CompletableFuture.runAsync( () -> {
             System.out.println("Running Thread : "+Thread.currentThread().getName());
         });
-        System.out.println("Main Thread Continue...");
+        System.out.println("Main Continue...");
+        Thread.sleep(1000);
     }
 }
